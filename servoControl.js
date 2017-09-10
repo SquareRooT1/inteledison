8c9b5f3e3c5a4dda190a243c3fde2cfca860ce55
muhammed <Frozen@muhammed-MacBook-Air-7.local>
2017-09-10 20:03:29 +0300

    servo check

--- servoControl.js
+++ servoControl.js
@@ -13,6 +13,7 @@ board.on("ready", function() {
   // Plug the Servo module
   // into the Grove Shield's D5 jack
   var servo = new five.Servo(5);
+  var led = new five.Led(8);
 
   // Set scaling of the Rotary angle
   // sensor's output to 0-180° (8-bit)
@@ -20,6 +21,11 @@ board.on("ready", function() {
   // degrees corresponding to the
   // value of the sensor
   rotary.scale(0, 180).on("change", function() {
+    if(this.value > 90){
+       led.on();
+    }else{
+      led.off();
+    }
     servo.to(this.value);
   });
 });
