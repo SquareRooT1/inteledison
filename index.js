43e70d5f4854d0beeaaf7016a9559d18a21e6f07
muhammed <Frozen@muhammed-MacBook-Air-7.local>
2017-09-10 15:08:05 +0300

    test flight blink

---
+++ index.js
@@ -0,0 +1,20 @@
+var five = require("johnny-five");
+var Edison = require("edison-io");
+var board = new five.Board({
+  io: new Edison()
+});
+
+board.on("ready", function() {
+
+
+  // Select an LED from the kit
+  // (red, green, blue) and insert
+  // it into the LED module, with
+  // the long pin in + and short
+  // pin in -.
+  var led = new five.Led(8);
+
+  // This will blink the LED over
+  // 500ms periods.
+  led.blink(500);
+});
